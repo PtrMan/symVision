1dd8f99b56b67cd3720b771f9a47cf4e3549c4cf
PtrMan <robertw89@googlemail.com>
2014-12-11 18:16:07 +0100

    * parameters

--- src/bpsolver/Parameters.java
+++ src/bpsolver/Parameters.java
@@ -19,4 +19,14 @@ public class Parameters
         public final static float MINIMALACTIVATIONTOSUMRATIO = 0.0f; // minimal ratio of the activation of an detector to the sum of all detectors to not get discarded
         public final static float LOCKINGACTIVATION = LOCKINGACTIVATIONMSESCALE*MAXMSE+1.0f+5.5f; // the minimal activation of a detector to get locked
     }
+    
+    public static class ProcessE
+    {
+        public final static int NEIGHTBORHOODSEARCHRADIUS = 10;
+    }
+    
+    public static class ProcessH
+    {
+        public final static float MAXDISTANCEFORCANDIDATEPOINT = 3.0f;
+    }
 }
