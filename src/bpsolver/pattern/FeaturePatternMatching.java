74d610336f4a67edcfaaa1aee3172a08ea741b2e
PtrMan <robertw89@googlemail.com>
2014-12-14 18:56:11 +0100

    * added remaining pattern matching facility, incomplete

--- src/bpsolver/pattern/FeaturePatternMatching.java
+++ src/bpsolver/pattern/FeaturePatternMatching.java
@@ -1,7 +1,20 @@
-package bpsolver;
+package bpsolver.pattern;
 
+import FargGeneral.network.Link;
+import FargGeneral.network.Node;
+import bpsolver.FeatureStatistics;
+import bpsolver.HardParameters;
+import bpsolver.NetworkHandles;
+import bpsolver.nodes.FeatureNode;
+import bpsolver.nodes.NodeTypes;
+import bpsolver.nodes.PlatonicPrimitiveInstanceNode;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import misc.Assert;
 import org.apache.commons.math3.analysis.UnivariateFunction;
 import org.apache.commons.math3.analysis.integration.BaseAbstractUnivariateIntegrator;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * class which encapuslates feature matching for learning/matching in the workspace/ltm
@@ -81,6 +94,128 @@ public class FeaturePatternMatching
         else
         {
             return matchSameTypeNumeroistyWithF1AndF2NotEqual1(f1, numeriosity1, f2, numeriosity2);
+        }
+    }
+    
+    // matches two (PlatonicPrimitiveInstanceNode for now) against each other and returns the matching degree
+    // takes the activation/priming inside the LTM into account to do so
+    // see foundalis disertation 8.2.3 to see how it is compared
+    public float matchAnyNonRecursive(Node nodeA, Node nodeB, NetworkHandles networkHandles)
+    {
+        // the type is FeatureNode.featureTypeNode
+        // only if two elements are in the array the type is 
+        Map<Node, ArrayList<FeatureNode>> featureNodesByType;
+        
+        // check that the nodes are PlatonicOrimitiveInstanceNode's
+        // this can be removed if we need functionality which isn't constrained that way
+        // for now its enought
+        // if we remove this we need for each node a IS link(and the best would be an accelerated type based access)
+        Assert.Assert(nodeA.type == NodeTypes.EnumType.PLATONICPRIMITIVEINSTANCENODE.ordinal(), "");
+        Assert.Assert(nodeB.type == NodeTypes.EnumType.PLATONICPRIMITIVEINSTANCENODE.ordinal(), "");
+        
+        // we return zero here because if the type is different it is ill-defined (as defined for now)
+        if( !( ((PlatonicPrimitiveInstanceNode)nodeA).primitiveNode.equals(((PlatonicPrimitiveInstanceNode)nodeB).primitiveNode) ) )
+        {
+            return 0.0f;
+        }
+        
+        featureNodesByType = new HashMap<>();
+        
+        getAllFeatureNodesAndAddToMap(nodeA, featureNodesByType);
+        getAllFeatureNodesAndAddToMap(nodeB, featureNodesByType);
+        
+        return matchAndWeightFeatureNodesByType(featureNodesByType, networkHandles);
+    }
+    
+    
+    // TOOO matchAnyRecursive
+    // recursive until lower bound
+    
+    
+    /** helper for matchAnyNonRecursive
+     * 
+     * goes through the map and weights it after the activation in LTM
+     * non common FeatureNodes (where the count is != 2) are weighted with zero
+     * 
+     */
+    private float matchAndWeightFeatureNodesByType(Map<Node, ArrayList<FeatureNode>> featureNodesByType,  NetworkHandles networkHandles)
+    {
+        float weightSum, upperSum;
+        
+        weightSum = 0.0f;
+        upperSum = 0.0f;
+        
+        if( featureNodesByType.keySet().isEmpty() )
+        {
+            return 0.0f;
+        }
+        
+        for( Node iterationTypeNode : featureNodesByType.keySet() )
+        {
+            float weight;
+            float featureDistance;
+            ArrayList<FeatureNode> featureNodesForType;
+            
+            featureNodesForType = featureNodesByType.get(iterationTypeNode);
+            
+            if( featureNodesForType.size() == 2 )
+            {
+                featureDistance = matchSameTypeNonNumerosity(featureNodesForType.get(0).statistics, featureNodesForType.get(1).statistics);
+            }
+            else
+            {
+                featureDistance = 0.0f;
+            }
+            
+            weight = (iterationTypeNode.activiation + getSystemWeightOfPlatonicFeatureType(iterationTypeNode, networkHandles)) * 0.5f;
+            
+            upperSum += (featureDistance * weight);
+            weightSum += weight;
+        }
+        
+        return upperSum / weightSum;
+    }
+    
+    // helper for
+    private static float getSystemWeightOfPlatonicFeatureType(Node featureType, NetworkHandles networkHandles)
+    {
+        if( featureType.equals(networkHandles.lineSegmentFeatureLineLengthPrimitiveNode) )
+        {
+            return HardParameters.FeatureWeights.LINESEGMENTFEATURELINELENGTH;
+        }
+        return 0.0f;
+    }
+    
+    // helper for matchAnyNonRecursive
+    private static void getAllFeatureNodesAndAddToMap(Node node, Map<Node, ArrayList<FeatureNode>> featureNodesByType)
+    {
+        ArrayList<Link> attributeLinksFromNode;
+        
+        attributeLinksFromNode = node.getLinksByType(Link.EnumType.HASATTRIBUTE);
+        
+        for( Link iterationAttributeLink : attributeLinksFromNode )
+        {
+            FeatureNode targetFeatureNode;
+            
+            if( iterationAttributeLink.target.type != NodeTypes.EnumType.FEATURENODE.ordinal() )
+            {
+                continue;
+            }
+            
+            targetFeatureNode = (FeatureNode)iterationAttributeLink.target;
+            
+            if( featureNodesByType.containsKey(targetFeatureNode.featureTypeNode) )
+            {
+                featureNodesByType.get(targetFeatureNode.featureTypeNode).add(targetFeatureNode);
+            }
+            else
+            {
+                ArrayList<FeatureNode> createdFeatureNodeList;
+                
+                createdFeatureNodeList = new ArrayList<>();
+                createdFeatureNodeList.add(targetFeatureNode);
+                featureNodesByType.put(targetFeatureNode.featureTypeNode, createdFeatureNodeList);
+            }
         }
     }
     
---
+++ src/bpsolver/pattern/Pattern.java
@@ -0,0 +1,19 @@
+package bpsolver.pattern;
+
+import FargGeneral.network.Node;
+import java.util.ArrayList;
+
+/**
+ *
+ * is a statistical sumarry of a group formed by exemplars
+ */
+public class Pattern
+{
+    // must be Node
+    public ArrayList<Node> exemplars;
+        
+    //public FeatureStatistics getStatisticalSumOfExemplars()
+    //{
+    
+    //}
+}
