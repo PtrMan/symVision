2c795cf0ebf95895885d95f1af42b499eeed028f
PtrMan <robertw89@googlemail.com>
2014-12-26 21:36:51 +0100

    * modified distinct utility so it is more universal

--- src/math/DistinctUtility.java
+++ src/math/DistinctUtility.java
@@ -9,18 +9,23 @@ public class DistinctUtility
     {
         int randomNumber;
         ArrayList<Integer> temporaryList;
+        ArrayList<Integer> additionalNumbers;
         
         randomNumber = random.nextInt(max);
         temporaryList = new ArrayList<>();
         temporaryList.add(randomNumber);
         
-        getDisjuctNumbersTo(random, temporaryList, 1, max);
+        additionalNumbers = getDisjuctNumbersTo(random, temporaryList, 1, max);
+        temporaryList.addAll(additionalNumbers);
         return temporaryList;
     }
     
-    public static void getDisjuctNumbersTo(Random random, ArrayList<Integer> numbers, int count, int max)
+    public static ArrayList<Integer> getDisjuctNumbersTo(Random random, ArrayList<Integer> numbers, int count, int max)
     {
         int counter;
+        ArrayList<Integer> disjunctNumbers;
+        
+        disjunctNumbers = new ArrayList<Integer>();
         
         for( counter = 0; counter < count; counter++ )
         {
@@ -29,13 +34,15 @@ public class DistinctUtility
                 int randomNumber;
             
                 randomNumber = random.nextInt(max);
-                if( !numbers.contains(randomNumber) )
+                if( !numbers.contains(randomNumber) && !disjunctNumbers.contains(randomNumber) )
                 {
-                    numbers.add(randomNumber);
+                    disjunctNumbers.add(randomNumber);
                     break;
                 }
             }
             
         }
+        
+        return disjunctNumbers;
     }
 }
