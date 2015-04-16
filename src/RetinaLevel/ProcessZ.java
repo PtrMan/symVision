177d08f78e9fbe0d847ab9670c4d687e61ad4e44
PtrMan <robertw89@googlemail.com>
2015-04-16 16:50:16 +0200

    * added ProcessZ (doesn't compile jet)

--- src/RetinaLevel/ProcessZ.java
+++ src/RetinaLevel/ProcessZ.java
@@ -2,11 +2,13 @@ package RetinaLevel;
 
 import Datastructures.Map2d;
 import Datastructures.Vector2d;
+import Visual.Map2dBinary;
+import Visual.Map2dConverter;
+import Visual.Map2dTransform;
+import misc.GaussianBlur;
 
 public class ProcessZ
 {
-
-
     public void setup(Vector2d<Integer> imageSize)
     {
         this.imageSize = imageSize;
@@ -14,8 +16,32 @@ public class ProcessZ
 
     public void process(Map2d<Boolean> input)
     {
+        Map2d<Float> floatNotMagnified;
+        Map2d<Float> floatMagnified;
+        Map2d<Float> floatMagnifiedBlured;
+        Map2d<Boolean> tempResult;
 
+        floatNotMagnified = new Map2d<>(imageSize.x, imageSize.y);
+        floatMagnified = new Map2d<>(imageSize.x*2, imageSize.y*2);
+        tempResult = new Map2d<>(imageSize.x, imageSize.y);
+
+        Map2dConverter.booleanToFloat(input, floatNotMagnified);
+        map2dTranform.magnify(floatNotMagnified, floatMagnified, 2);
+        floatMagnifiedBlured = GaussianBlur.blur(2, floatMagnified);
+        Map2dConverter.floatToBoolean(floatMagnifiedBlured, tempResult, FLOATTOBOOLEANTHRESHOLD);
+        magnifiedOutput = Map2dBinary.corode(tempResult);
+    }
+
+    public Map2d<Boolean> getMagnifiedOutput()
+    {
+        return magnifiedOutput;
     }
 
     private Vector2d<Integer> imageSize;
+
+    private Map2d<Boolean> magnifiedOutput;
+
+    private Map2dTransform<Float> map2dTranform = new Map2dTransform<>();
+
+    private final float FLOATTOBOOLEANTHRESHOLD = 0.6f;
 }
