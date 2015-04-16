d364c4f8d76b63211dcb19678374f8cc8450b1ab
PtrMan <robertw89@googlemail.com>
2015-04-16 16:27:50 +0200

    * src/Visual/Map2dTransform.java implemented

--- src/Visual/Map2dTransform.java
+++ src/Visual/Map2dTransform.java
@@ -1,7 +1,32 @@
 package Visual;
 
-/**
- * Created by r0b3 on 16.04.15.
- */
-public class Map2dTransform {
+import Datastructures.Map2d;
+
+public class Map2dTransform<Type>
+{
+    public void magnify(Map2d<Type> input, Map2d<Type> output, int factor)
+    {
+        int ix, iy;
+
+        for( iy = 0; iy < input.getLength(); iy++ )
+        {
+            for( ix = 0; ix < input.getWidth(); ix++ )
+            {
+                writeMagnified(output, ix, iy, input.readAt(ix, iy), factor);
+            }
+        }
+    }
+
+    private void writeMagnified(Map2d<Type> output, int ix, int iy, Type value, int factor)
+    {
+        int writeX, writeY;
+
+        for( writeY = 0; writeY < factor; writeY++ )
+        {
+            for( writeX = 0; writeX < factor; writeX++ )
+            {
+                output.setAt(ix*factor + writeX, iy*factor + writeY, value);
+            }
+        }
+    }
 }
