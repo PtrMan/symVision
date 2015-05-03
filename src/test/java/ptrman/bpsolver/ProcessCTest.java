178beae4367b139a25ec445ab5bce30834b39654
PtrMan <robertw89@googlemail.com>
2015-05-03 21:46:54 +0200

    * added ProcessC test, incomplete, without asserts

---
+++ src/test/java/ptrman/bpsolver/ProcessCTest.java
@@ -0,0 +1,67 @@
+package ptrman.bpsolver;
+
+import org.junit.Test;
+import ptrman.Datastructures.Vector2d;
+import ptrman.levels.retina.ProcessA;
+import ptrman.levels.retina.ProcessC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class ProcessCTest {
+    @Test
+    public void testProcessC() {
+        List<ProcessA.Sample> samples = new ArrayList<>();
+
+        ProcessA.Sample createdSample;
+
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(0, 0));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(1, 0));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(2, 0));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(0, 1));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(1, 1));
+        createdSample.altitude = (float)Math.sqrt(2.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(2, 1));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(0, 2));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(1, 2));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+        createdSample = new ProcessA.Sample(new Vector2d<>(2, 2));
+        createdSample.altitude = (float)Math.sqrt(0.0f);
+        samples.add(createdSample);
+
+
+        ProcessC processC = new ProcessC();
+
+        processC.process(samples);
+
+
+    }
+}
