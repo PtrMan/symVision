11cef8ffb73e026ce33b4398094211520f4b971d
PtrMan <robertw89@googlemail.com>
2015-05-04 23:54:17 +0200

    * Bresenham algorithm

---
+++ src/main/java/ptrman/Algorithms/Bresenham.java
@@ -0,0 +1,48 @@
+package ptrman.Algorithms;
+
+import ptrman.Datastructures.Vector2d;
+
+/**
+ * Bresenham algorithms
+ *
+ * for cirlce see german version of
+ * http://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+ */
+public class Bresenham {
+    public interface IDrawer {
+        void set(final Vector2d<Integer> position);
+    }
+
+    public static void rasterCircle(final Vector2d<Integer> position, final int radius, IDrawer drawer) {
+        int f = 1 - radius;
+        int ddF_x = 0;
+        int ddF_y = -2 * radius;
+        int x = 0;
+        int y = radius;
+
+        drawer.set(new Vector2d<>(position.x, position.y + radius));
+        drawer.set(new Vector2d<>(position.x, position.y - radius));
+        drawer.set(new Vector2d<>(position.x + radius, position.y));
+        drawer.set(new Vector2d<>(position.x - radius, position.y));
+
+        while(x < y) {
+            if(f >= 0) {
+                y--;
+                ddF_y += 2;
+                f += ddF_y;
+            }
+            x++;
+            ddF_x += 2;
+            f += ddF_x + 1;
+
+            drawer.set(new Vector2d<>(position.x + x, position.y + y));
+            drawer.set(new Vector2d<>(position.x - x, position.y + y));
+            drawer.set(new Vector2d<>(position.x + x, position.y - y));
+            drawer.set(new Vector2d<>(position.x - x, position.y - y));
+            drawer.set(new Vector2d<>(position.x + y, position.y + x));
+            drawer.set(new Vector2d<>(position.x - y, position.y + x));
+            drawer.set(new Vector2d<>(position.x + y, position.y - x));
+            drawer.set(new Vector2d<>(position.x - y, position.y - x));
+        }
+    }
+}
