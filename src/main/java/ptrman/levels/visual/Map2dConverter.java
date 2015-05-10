b8bccaf10abad55dca58e194faa4fc37141b75d0
PtrMan <robertw89@googlemail.com>
2015-05-10 22:03:05 +0200

    * refactoring

--- src/main/java/ptrman/levels/visual/Map2dConverter.java
+++ src/main/java/ptrman/levels/visual/Map2dConverter.java
@@ -2,54 +2,35 @@ package ptrman.levels.visual;
 
 import ptrman.Datastructures.IMap2d;
 
-public class Map2dConverter
-{
-    public static void booleanToFloat(IMap2d<Boolean> input, IMap2d<Float> output)
-    {
-        int ix, iy;
-
-        for( iy = 0; iy < input.getWidth(); iy++ )
-        {
-            for( ix = 0; ix < input.getLength(); ix++ )
-            {
-                boolean valueAtPosition;
-
-                valueAtPosition = input.readAt(ix, iy);
+public class Map2dConverter {
+    public static void booleanToFloat(final IMap2d<Boolean> input, IMap2d<Float> output) {
+        for( int iy = 0; iy < input.getWidth(); iy++ ) {
+            for( int ix = 0; ix < input.getLength(); ix++ ) {
+                boolean valueAtPosition = input.readAt(ix, iy);
                 output.setAt(ix, iy, convertBooleanToFloat(valueAtPosition));
             }
         }
     }
 
-    public static void floatToBoolean(IMap2d<Float> input, IMap2d<Boolean> output, float threshold)
-    {
-        int ix, iy;
-
-        for( iy = 0; iy < input.getWidth(); iy++ )
-        {
-            for( ix = 0; ix < input.getLength(); ix++ )
-            {
-                float valueAtPosition;
-
-                valueAtPosition = input.readAt(ix, iy);
+    public static void floatToBoolean(final IMap2d<Float> input, IMap2d<Boolean> output, final float threshold) {
+        for( int iy = 0; iy < input.getWidth(); iy++ ) {
+            for( int ix = 0; ix < input.getLength(); ix++ ) {
+                float valueAtPosition = input.readAt(ix, iy);
                 output.setAt(ix, iy, floatAboveThreshold(valueAtPosition, threshold));
             }
         }
     }
 
-    private static float convertBooleanToFloat(boolean value)
-    {
-        if( value )
-        {
+    private static float convertBooleanToFloat(final boolean value) {
+        if( value ) {
             return 1.0f;
         }
-        else
-        {
+        else {
             return 0.0f;
         }
     }
 
-    private static boolean floatAboveThreshold(float value, float threshold)
-    {
+    private static boolean floatAboveThreshold(final float value, final float threshold) {
         return value > threshold;
     }
 }
