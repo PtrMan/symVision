ee596cc5155233260da40a5594ca109ed3a9a601
PtrMan <robertw89@googlemail.com>
2015-05-10 22:11:07 +0200

    * [visual/Map2dConverter] bug fixes

--- src/main/java/ptrman/levels/visual/Map2dConverter.java
+++ src/main/java/ptrman/levels/visual/Map2dConverter.java
@@ -4,8 +4,8 @@ import ptrman.Datastructures.IMap2d;
 
 public class Map2dConverter {
     public static void booleanToFloat(final IMap2d<Boolean> input, IMap2d<Float> output) {
-        for( int iy = 0; iy < input.getWidth(); iy++ ) {
-            for( int ix = 0; ix < input.getLength(); ix++ ) {
+        for( int iy = 0; iy < input.getLength(); iy++ ) {
+            for( int ix = 0; ix < input.getWidth(); ix++ ) {
                 boolean valueAtPosition = input.readAt(ix, iy);
                 output.setAt(ix, iy, convertBooleanToFloat(valueAtPosition));
             }
@@ -13,8 +13,8 @@ public class Map2dConverter {
     }
 
     public static void floatToBoolean(final IMap2d<Float> input, IMap2d<Boolean> output, final float threshold) {
-        for( int iy = 0; iy < input.getWidth(); iy++ ) {
-            for( int ix = 0; ix < input.getLength(); ix++ ) {
+        for( int iy = 0; iy < input.getLength(); iy++ ) {
+            for( int ix = 0; ix < input.getWidth(); ix++ ) {
                 float valueAtPosition = input.readAt(ix, iy);
                 output.setAt(ix, iy, floatAboveThreshold(valueAtPosition, threshold));
             }
