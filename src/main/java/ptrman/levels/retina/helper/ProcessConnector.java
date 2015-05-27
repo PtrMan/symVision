558a081699c5846d29e41cbe0db085bf927219ba
PtrMan <robertw89@googlemail.com>
2015-05-27 16:23:46 +0200

    * added ProcessConnector (code is not refactored to use it)

---
+++ src/main/java/ptrman/levels/retina/helper/ProcessConnector.java
@@ -0,0 +1,71 @@
+package ptrman.levels.retina.helper;
+
+import ptrman.misc.Assert;
+
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Connector between two Processes.
+ *
+ * Can be either in WORKSPACE or QUEUE mode.
+ */
+public class ProcessConnector<Type> {
+
+    public enum EnumMode {
+        WORKSPACE,
+        QUEUE,
+        PRIMARY_QUEUE // workspace is used as a "logger", behaves like a queue
+    }
+
+    private ProcessConnector(Queue<Type> queueImplementation, List<Type> workspaceImplementation, EnumMode mode) {
+        this.queue = queueImplementation;
+        this.workspace = workspaceImplementation;
+        this.mode = mode;
+    }
+
+    public List<Type> getWorkspace() {
+        Assert.Assert(mode == EnumMode.WORKSPACE || mode == EnumMode.PRIMARY_QUEUE, "");
+        Assert.Assert(workspace != null, "");
+        return workspace;
+    }
+
+    public void add(Type element) {
+        if( mode == EnumMode.WORKSPACE || mode == EnumMode.PRIMARY_QUEUE ) {
+            workspace.add(element);
+        }
+
+        if( mode == EnumMode.QUEUE || mode == EnumMode.PRIMARY_QUEUE ) {
+            queue.add(element);
+        }
+    }
+
+    public Type poll() {
+        Assert.Assert( mode == EnumMode.QUEUE || mode == EnumMode.PRIMARY_QUEUE, "");
+        return queue.poll();
+    }
+
+    public int getSize() {
+        if( mode == EnumMode.WORKSPACE ) {
+            return workspace.size();
+        }
+        else {
+            return queue.size();
+        }
+    }
+
+    public void flush() {
+        if( workspace != null ) {
+            workspace.clear();
+        }
+
+        if ( queue != null ) {
+            queue.clear();
+        }
+    }
+
+    private EnumMode mode;
+
+    private Queue<Type> queue;
+    private List<Type> workspace;
+}
