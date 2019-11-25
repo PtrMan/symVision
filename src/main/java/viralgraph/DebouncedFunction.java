7bea36db8e798a954bb9015ee8cf6d63dc6778b0
PtrMan <1067920+PtrMan@users.noreply.github.com>
2019-11-25 18:41:14 +0100

    remove: ViralGraph

--- src/main/java/viralgraph/DebouncedFunction.java
+++
@@ -1,9 +0,0 @@
-package viralgraph;
-
-
-import java.util.function.Function;
-
-/** TODO forms basis for realtime async graph */
-abstract public class DebouncedFunction<X,Y> implements Function<X,Y> {
-	long lastComputed  = Long.MIN_VALUE;
-}
